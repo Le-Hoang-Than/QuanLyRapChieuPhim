35e4704e472f451ca4c8ea696e7d957780b0b210
toru06-huy <huy10072004@gmail.com@gmail.com>
2025-11-12 18:33:56 +0700

    QuanLyRapChieuPhim

---
+++ QuanLyRapChieuPhim/src/test/java/com/example/cinema_back_end/CinemaBackEndApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.cinema_back_end;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+class CinemaBackEndApplicationTests {
+}
