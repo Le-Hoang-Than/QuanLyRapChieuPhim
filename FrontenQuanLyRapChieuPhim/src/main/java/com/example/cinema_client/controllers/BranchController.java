85d66ec91cfe031316eae90529bb87f29ee95ece
toru06-huy <huy10072004@gmail.com@gmail.com>
2025-12-18 00:53:42 +0700

    Them giao dien quan ly chi nhanh

--- FrontenQuanLyRapChieuPhim/src/main/java/com/example/cinema_client/controllers/BranchController.java
+++ FrontenQuanLyRapChieuPhim/src/main/java/com/example/cinema_client/controllers/BranchController.java
@@ -1,15 +1,19 @@
 package com.example.cinema_client.controllers;
 
 import com.example.cinema_client.constants.Api;
+import com.example.cinema_client.models.BranchDTO;
 import com.example.cinema_client.models.JwtResponseDTO;
-import com.example.cinema_client.models.TicketDTO;
 import com.example.cinema_client.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -19,15 +23,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/tickets")
-public class TicketController {
+@RequestMapping("/branches")
+public class BranchController {
     @Autowired
     private RestTemplate restTemplate;
 
-    public static String API_GET_TICKETS = Api.baseURL+"/api/tickets";
-    @GetMapping("/history")
-    public String displayHistoryPage(Model model, HttpServletRequest request){
+    public static String apiGetBranches = Api.baseURL+"/api/branches";
+
+    @GetMapping
+    public String displayBranchesPage(@RequestParam Integer movieId, Model model, HttpServletRequest request){
+        // Gắn movie id vào session lát sau dùng tiếp để tìm ra lịch xem cụ thể dựa trên movie id đó
         HttpSession session = request.getSession();
+        session.setAttribute("movieId",movieId);
+
         // Gắn access token jwt vào header để gửi kèm request
         HttpHeaders headers = new HttpHeaders();
         headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
@@ -35,21 +43,25 @@ public class TicketController {
         headers.set(HttpHeaders.AUTHORIZATION,"Bearer "+jwtResponseDTO.getAccessToken());
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
-        // Gọi api lấy ra lịch được chọn
-        String urlTemplate = UriComponentsBuilder.fromHttpUrl(API_GET_TICKETS)
-                .queryParam("userId", "{userId}")
+        // Truyền tham số movieId vào query string rồi gửi request
+        String urlTemplate = UriComponentsBuilder.fromHttpUrl(apiGetBranches)
+                .queryParam("movieId", "{movieId}")
                 .encode()
                 .toUriString();
-        Map<String,String> listRequestParam = new HashMap<>();
-        listRequestParam.put("userId", jwtResponseDTO.getId()+"");
+        Map<String, Integer> params = new HashMap<>();
+        params.put("movieId", movieId);
 
-        ResponseEntity<TicketDTO[]> response = restTemplate.exchange(urlTemplate, HttpMethod.GET,entity,TicketDTO[].class
-        ,listRequestParam);
-
-        TicketDTO[] listTickets = response.getBody();
-
-        model.addAttribute("listTickets",listTickets);
+        HttpEntity<BranchDTO[]> response = restTemplate.exchange(
+                urlTemplate,
+                HttpMethod.GET,
+                entity,
+                BranchDTO[].class,
+                params
+        );
+        BranchDTO[] branchDTOS = response.getBody();
+        model.addAttribute("branches",branchDTOS);
         model.addAttribute("user",new User());
-        return "history";
+        return "branches";
+
     }
 }
